bdd4421f33d57ab2770612b9b70292dcce090889
madebyvikrin <llpllplpvh@mail.ru>
2023-02-13 03:38:52 +0500

    add some variables in js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let memory = [];
+
+const inputs = document.querySelectorAll('[id*="input"]');
+const output = document.querySelector('#output');
+const operatorsBtns = document.querySelectorAll('[id*="operator"]');
+const toCountBtn = document.querySelector('.inputs-block__submit-btn');
+const random2ndNumberBtn = document.querySelector('.inputs-block__random-2nd');
+const clearOutputBtn = document.querySelector('#clearButton');
+const outputToInput1 = document.querySelector('#moveValueButton');
