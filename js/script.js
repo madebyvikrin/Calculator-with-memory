128e55f78e8ce44c1a170ada1ee6d71c13a12806
madebyvikrin <llpllplpvh@mail.ru>
2023-02-13 17:53:56 +0500

    beta 1.0.1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 "use strict";
 
-let memory = [];
-let operator;
-
 const inputs = document.querySelectorAll('[id*="input"]');
 const output = document.querySelector("#output");
 const operatorsBtns = document.querySelectorAll('[id*="operator"]');
@@ -11,60 +8,110 @@ const random2ndNumberBtn = document.querySelector(".inputs-block__random-2nd");
 const clearOutputBtn = document.querySelector("#clearButton");
 const outputToInput1 = document.querySelector("#moveValueButton");
 
-operatorsBtns[0].addEventListener("click", () => {
-	operator = " + ";
-});
-operatorsBtns[1].addEventListener("click", () => {
-	operator = " - ";
-});
-operatorsBtns[2].addEventListener("click", () => {
-	operator = " * ";
-});
-operatorsBtns[3].addEventListener("click", () => {
-	operator = " / ";
-});
+let operator;
 
-const getValues = () => {
-	let firstNumber = inputs[0].value;
-	let secondNumber = inputs[1].value;
-	return [firstNumber, secondNumber];
+const setOperatorsBtns = () => {
+	operatorsBtns[0].addEventListener("click", () => {
+		for (let btn of operatorsBtns) {
+			btn.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+		}
+
+		operatorsBtns[0].style.border = "2px solid #00FF19";
+
+		operator = " + ";
+	});
+
+	operatorsBtns[1].addEventListener("click", () => {
+		for (let btn of operatorsBtns) {
+			btn.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+		}
+
+		operatorsBtns[1].style.border = "2px solid #00FF19";
+
+		operator = " - ";
+	});
+
+	operatorsBtns[2].addEventListener("click", () => {
+		for (let btn of operatorsBtns) {
+			btn.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+		}
+
+		operatorsBtns[2].style.border = "2px solid #00FF19";
+
+		operator = " * ";
+	});
+
+	operatorsBtns[3].addEventListener("click", () => {
+		for (let btn of operatorsBtns) {
+			btn.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+		}
+
+		operatorsBtns[3].style.border = "2px solid #00FF19";
+
+		operator = " / ";
+	});
 };
+
+setOperatorsBtns();
 
 const getExpression = () => {
+	const getValues = () => {
+		let firstNumberStr = inputs[0].value;
+		let secondNumberStr = inputs[1].value;
+		return [firstNumberStr, secondNumberStr];
+	};
+
 	let numbers = getValues();
+
 	return numbers[0] + operator + numbers[1];
-};
-
-const calculate = () => {
-	let expression = getExpression();
-
-	let pos1 = expression.indexOf(" ");
-	let pos2 = pos1 + 3;
-
-	let firstNumber = expression.slice(0, pos1);
-	let secondNumber = expression.slice(pos2);
-	let operator = expression.charAt(pos1 + 1);
-
-	switch (operator) {
-		case "+":
-			return `${+firstNumber + +secondNumber}`;
-
-		case "-":
-			return `${+firstNumber - +secondNumber}`;
-
-		case "*":
-			return `${+firstNumber * +secondNumber}`;
-
-		case "/":
-			return `${Math.floor(+firstNumber / +secondNumber)}`;
-
-		default:
-			throw "Error: operator undefined";
-	}
 };
 
 const showResultInOutput = () => {
 	output.innerHTML = `${calculate()}`;
+
+	function calculate() {
+		let expression = getExpression();
+
+		let pos1 = expression.indexOf(" ");
+		let pos2 = pos1 + 3;
+
+		let firstNumber = expression.slice(0, pos1);
+		let secondNumber = expression.slice(pos2);
+		let operator = expression.charAt(pos1 + 1);
+
+		switch (operator) {
+			case "+":
+				return `${+firstNumber + +secondNumber}`;
+
+			case "-":
+				return `${+firstNumber - +secondNumber}`;
+
+			case "*":
+				return `${+firstNumber * +secondNumber}`;
+
+			case "/":
+				return `${+firstNumber / +secondNumber}`;
+
+			default:
+				for (let btn of operatorsBtns) {
+					btn.style.border = "2px solid #FF0000";
+				}
+
+				return "";
+		}
+	}
+};
+
+const clearAll = () => {
+	output.innerHTML = "";
+	inputs[0].value = "";
+	inputs[1].value = "";
+};
+
+const moveRelultToFirstValue = () => {
+	inputs[0].value = `${output.innerHTML}`;
 };
 
 toCountBtn.addEventListener("click", showResultInOutput);
+clearOutputBtn.addEventListener("click", clearAll);
+outputToInput1.addEventListener("click", moveRelultToFirstValue);
